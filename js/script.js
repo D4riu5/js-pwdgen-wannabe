4242f441a422182a6a535d1cfdf784ef709e5e41
D4riu5 <R3iji94@gmail.com>
2022-12-16 15:09:42 +0100

    added return false , when pressing cancel on prompt, so user cannot bypass a required field

--- js/script.js
+++ js/script.js
@@ -5,16 +5,28 @@ function userInput() {
         alert('Entry Required');
         return false;
       }
+    if (firstName == null) {
+    alert('Entry Required');
+    return false;
+    }  
     const lastName = prompt("Enter your last name");
     if (lastName == "") {
         alert('Entry Required');
         return false;
       }
+    if (lastName == null) {
+    alert('Entry Required');
+    return false;
+    }    
     const favColor = prompt("Enter your favorite color");
     if (favColor == "") {
         alert('Entry Required');
         return false;
       }
+    if (favColor == null) {
+    alert('Entry Required');
+    return false;
+    }  
     let pwd = firstName + lastName + favColor + '21'
     document.getElementById("showPWD").value = pwd;
 }
